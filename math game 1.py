6ce006dbbb4899bba174a92beb11601fd899193e
sainathkumar <69648897+sainathkumar@users.noreply.github.com>
2020-08-14 00:40:00 +0530

    Math game

---
+++ math game 1.py
@@ -0,0 +1,13 @@
+a = int(input('put the two digit number '))
+b = 2
+c = (a-2)
+print("predected answer is",b,c)
+d = int( input("now tell one more number"))
+e = 99 - d
+print("this is my num",e)
+f = int(input("one more time tell num"))
+g = 99 - f
+print("this is my num",g)
+h = a+d+e+f+g
+print('add this num see magic',a,d,e,f,g)
+print("this is final sum",h)
